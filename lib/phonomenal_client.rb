23f2c40e96545e1a7a325b6ebafdd2c51962c8b9
Venkatakrishnan Ganesh <venkatk@delta18.com>
2025-05-24 22:56:25 +0530

    Initial commit

---
+++ lib/phonomenal_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "phonomenal_client/version"
+
+module PhonomenalClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/phonomenal_client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PhonomenalClient do
+  it "has a version number" do
+    expect(PhonomenalClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
