255c87e74a76b4f6eab1f836c5081453e39d9c72
Venkatakrishnan Ganesh <venkatk@delta18.com>
2025-10-14 20:45:59 +0530

    The API for lead management

--- lib/phonomenal/leads.rb
+++ lib/phonomenal/leads.rb
@@ -2,5 +2,41 @@
 
 module Phonomenal
   Leads = Struct.new(:client) do
+    def list(filter: nil)
+      Response.new(client.class.get(client.url_for("/leads"), query: { filter: filter }))
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def block
+    end
+
+    def restore
+    end
+
+    def bump
+    end
+
+    def unbump
+    end
+
+    def set_follow_up
+    end
+
+    def unset_follow_up
+    end
+
+    def reset
+    end
+
+    def bulk_create
+    end
+
+    def bulk_delete
+    end
   end
 end
