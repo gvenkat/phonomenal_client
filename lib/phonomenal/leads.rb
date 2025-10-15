15dc3417b6af95e5ff6c12cf240d20829e318b78
Venkatakrishnan Ganesh <venkatk@delta18.com>
2025-10-16 01:20:33 +0530

    Issue with the payload

--- lib/phonomenal/leads.rb
+++ lib/phonomenal/leads.rb
@@ -11,21 +11,21 @@ module Phonomenal
     end
 
     def create(object)
-      make_response :post, "/leads", body: { lead: object }
+      make_response :post, "/leads", body: { lead: object }.to_json
     end
 
     def update(lead_id, object)
-      make_response :put, "/leads/#{lead_id}", body: { lead: object }
+      make_response :put, "/leads/#{lead_id}", body: { lead: object }.to_json
     end
 
     %w[block unblock bump unbump reset unset_follow_up].each do |method_name|
       define_method method_name do |lead_id|
-        make_response :post, "/leads/#{lead_id}/#{method_name}", body: {}
+        make_response :post, "/leads/#{lead_id}/#{method_name}", body: {}.to_json
       end
     end
 
     def set_follow_up(lead_id, follow_up_at)
-      make_response :post, "/leads/#{lead_id}/set_follow_up", body: { follow_up_at: follow_up_at }
+      make_response :post, "/leads/#{lead_id}/set_follow_up", body: { follow_up_at: follow_up_at }.to_json
     end
   end
 end
